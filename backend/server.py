e3c2c1a792a10634999452bdd9890cb06f3ec42c
Jason Chan <jason27chan@gmail.com>
2019-10-26 13:22:51 -0400

    change endpoint name to match functionality

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/profile_info')
+def profile_info():
+    user = request.args.get('id')
     return 'Hello, World!'
 
 if __name__ == '__main__':
