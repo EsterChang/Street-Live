0144f99789d656bb4922ee2cd79873a2bbc855fb
Jason Chan <jason27chan@gmail.com>
2019-10-26 12:33:49 -0400

    implement some ideas for routing

---
+++ profile_page.js
@@ -0,0 +1,9 @@
+$(function() {
+    let splitUrl = window.location.pathname.split("/");
+    id = splitUrl[splitUrl.length - 1];
+
+    // $.get(apiUrl + id, function(data) {
+    //     $("#name").text = data.name;
+    //     ...
+    // });
+})
