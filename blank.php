86fe3f919739fbd48f3f68af07700bfa0cf78382
Ester Chang <esterchang9110@gmail.com>
2019-10-27 04:38:33 -0400

    add profile_page.php

--- blank.php
+++ blank.php
@@ -16,9 +16,9 @@ head('Home', true);
     <script src="js/bootstrap.js"></script>
     <script src="browse_page.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="browse_page.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="browse_page.css">
 
     <title>Browse Page</title>
 </head>
@@ -39,19 +39,18 @@ head('Home', true);
             
             <div class="card-deck">
                 <div class="card w-100">
-                    <iframe width="375" height="180" src="https://www.youtube.com/embed/zCuW4A57jac" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="300" height="180" src="https://www.youtube.com/embed/x--yddOolRQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
         
                 <div class="card w-100">
-                    <iframe width="375" height="180" src="https://www.youtube.com/embed/gfbRmTUXGs0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="300" height="180" src="https://www.youtube.com/embed/-ZTsgbrdoI8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
         
                 <div class="card w-100">
-                    <iframe width="375" height="180" src="https://www.youtube.com/embed/vMFXDxiUMRY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="300" height="180" src="https://www.youtube.com/embed/Imt0q-7LAVI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
-        
 
         <div class="row">
           <div class="col-md-12">
@@ -67,7 +66,7 @@ head('Home', true);
                             <img class="card-img-top" alt="" src="https://i.imgur.com/lMHGHYt.jpg" data-holder-rendered="true">
                         </a>
                         <div class="card-body">
-                                <h5 class="card-title"> test </h5>
+                                <h5 class="card-title"> Mary </h5>
                         </div>
                       </div>
                     </div>
@@ -116,132 +115,13 @@ head('Home', true);
 </body>
 </html>
 
-<script> 
-
-    // $('carousel').carousel();
-
-    // $('.carousel-control-prev.left').click(function() {
-    //     $('#myCarousel').carousel('prev');
-    // });
-
-    // $('.carousel-control-next.right').click(function() {
-    //     $('#myCarousel').carousel('next');
-    // });
-
-function initMap() {
-    var latitude = 33.248;
-    var longitude = -83.441;
-    var city = {lat: latitude, lng: longitude};
-  
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 8, 
-            center: city,
-            styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
-        });
-  
-    var marker = new google.maps.Marker({position: city, map: map});
-
-    var contentString = '<p>Performer: <b></b> Location: </p>';
-
-    var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-}
-
-</script>
-
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyApJJg6RSH7MlCgOAMH9s5xcvbdbiiuSkw&callback=initMap"></script>
 
 
 <?php
 footer(false);
 ?>
 
+
 <script> 
 
     // $('carousel').carousel();
@@ -255,14 +135,11 @@ footer(false);
     // });
 
 function initMap() {
-    var latitude = 33.248;
-    var longitude = -83.441;
-    var city = {lat: latitude, lng: longitude};
   
     var map = new google.maps.Map(
         document.getElementById('map'), {
             zoom: 8, 
-            center: city,
+            center: {lat: 33.011, lng: -82.984},
             styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
@@ -345,17 +222,51 @@ function initMap() {
           ]
         });
   
+    var city2 = {lat: 33.248, lng: -83.441};
+    var city = {lat: 33.011, lng: -82.984};
+    var city3 = {lat: 32.87, lng: -84.02};
     var marker = new google.maps.Marker({position: city, map: map});
-
-    var contentString = '<p>Performer: <b></b> Location: </p>';
-
+    var marker2 = new google.maps.Marker({position: city2, map: map});
+    var marker3 = new google.maps.Marker({position: city3, map: map});
+    var contentString1 = '<object width="425" height="344"><param name="movie" value="https://www.youtube.com/embed/x--yddOolRQ"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/UmFjNiiVk9w?fs=1" type="application/x-shockwave-flash" width="425" height="344" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
+    var contentString2 = '<object width="425" height="344"><param name="movie" value="https://www.youtube.com/embed/-ZTsgbrdoI8"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/UmFjNiiVk9w?fs=1" type="application/x-shockwave-flash" width="425" height="344" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
+    var contentString3 = '<object width="425" height="344"><param name="movie" value="https://www.youtube.com/embed/Imt0q-7LAVI"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/UmFjNiiVk9w?fs=1" type="application/x-shockwave-flash" width="425" height="344" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
     var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+      content: contentString1
+    });
+    var infowindow2 = new google.maps.InfoWindow({
+      content: contentString2
+    });
+    var infowindow3 = new google.maps.InfoWindow({
+      content: contentString3
+    });
 
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+
+    marker2.addListener('click', function() {
+      infowindow2.open(map, marker2);
+    });
+
+    marker3.addListener('click', function() {
+      infowindow3.open(map, marker3);
+    });
+
+    $.get("/libs/docs_in_collection.php", {collection: "Streams"}, function(data) {
+        console.log('data');
+        console.log(data);
+        let streamInfo = JSON.parse(data);
+        console.log(streamInfo);
+
+        // for (stream of streamInfo) {
+
+        // }
+    }).fail(function(err) {
+        console.log('error');
+        console.log(err);
+    });
+
 }
 
 </script>
---
+++ profile_page.php
@@ -0,0 +1,55 @@
+<?php
+require('template/top.php');
+head('Home', true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script
+    src="https://code.jquery.com/jquery-2.2.4.min.js"
+    integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+    crossorigin="anonymous"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://www.w3schools.com/w3css/4/w3.css" rel="stylesheet">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
+    <script src="frontend/profile_page.js"></script>
+    <link rel="stylesheet" type="text/css" href="/frontend/profile_page.css">
+    <link rel="stylesheet" type="text/css" href="../browse_page.css">
+     
+    <title>Profile Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-sm-6">
+                <div class="description">
+                    <img id="profile_picture" class="avatar" src="https://i.imgur.com/lMHGHYt.jpg">
+                    <h1 id="name">The Fat Cat</h1>
+                    <p id="bio">Fat. Cat.</p>
+                    <p id="city" class="subtext">Atlanta, GA</p>
+                    <p id="type" class="subtext">Animal</p>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div id="feed">
+            <div class="w3-panel w3-card-2">
+                <p class="subtext">Friday 13th</p>
+                <p class="">hello world</p>
+            </div>
+            <div class="w3-panel w3-card-2">
+                    <p class="subtext">Friday 13th</p>
+                    <p class="">hello world</p>
+                </div>
+        </div>
+</body>
+</html>
+
+<?php
+footer(false);
+?>
